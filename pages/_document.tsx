f4a1d53c4419ceb9c53334de013c157d68230096
ps <pablosr11@gmail.com>
2023-04-25 20:32:20 +0100

    add header

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,29 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>· wikiskim ·</title>
+        <meta
+          name="description"
+          content="The largest offline encyclopedia"
+          key="desc"
+        />
+        <meta property="og:title" content="Wikiskim" />
+        <meta
+          property="og:description"
+          content="For a focused reading experience"
+        />
+        <meta
+          property="og:image"
+          content="https://example.com/images/cool-page.jpg"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
