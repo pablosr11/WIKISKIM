393840d7255a9b85bb7601f34c44442128ec261e
ps <pablosr11@gmail.com>
2023-04-25 21:32:09 +0100

    no title in document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>· wikiskim ·</title>
         <meta
           name="description"
           content="The largest offline encyclopedia"
