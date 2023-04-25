ac06b59124ee417722bad6722baf01f455664f27
ps <pablosr11@gmail.com>
2023-04-25 19:50:23 +0100

    v0 with zip files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
+import { saveAs } from "file-saver";
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import { Inter } from "next/font/google";
+
+import JSZip from "jszip";
 
 const inter = Inter({ subsets: ["latin"] });
 const wikipediaUrlPattern = new RegExp(
@@ -7,8 +10,162 @@ const wikipediaUrlPattern = new RegExp(
 );
 
 export default function Home() {
-  async function dostuff(data: { wikiUrl: string }) {
-    console.log(data.wikiUrl);
+  async function getWikipediaHtml(url: URL): Promise<string> {
+    var zip = new JSZip();
+    const apiUrl = new URL("https://en.wikipedia.org/w/api.php");
+    const title = url.pathname.split("/").pop();
+    if (!title) throw new Error("missing title");
+
+    apiUrl.search = new URLSearchParams({
+      action: "parse",
+      format: "json",
+      prop: "text",
+      page: decodeURIComponent(title),
+      origin: "*",
+    }).toString();
+
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error(
+        `HTTP error! Status: ${response.status}, ${response.statusText}, ${apiUrl}`
+      );
+    }
+    const data = await response.json();
+    const page = data.parse;
+
+    // parse the html
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(page.text["*"], "text/html");
+
+    // remove all images
+    doc.querySelectorAll("img, style").forEach((el) => el.remove());
+
+    // remove all css classes
+    doc.querySelectorAll("*").forEach((el) => {
+      el.removeAttribute("class");
+    });
+
+    // remove links with : any time after /wiki/
+    doc.querySelectorAll("a").forEach((el) => {
+      const href = el.getAttribute("href");
+      if (href && href.includes("/wiki/")) {
+        const parts = href.split(":");
+        if (parts.length > 1) {
+          el.remove();
+        }
+      }
+    });
+
+    // urldecode all links
+    doc.querySelectorAll("a").forEach((el) => {
+      const href = el.getAttribute("href");
+      if (href) {
+        el.setAttribute("href", decodeURIComponent(href));
+      }
+    });
+
+    // remove all links except the ones for /wiki/ pages
+    doc.querySelectorAll("a").forEach((el) => {
+      const href = el.getAttribute("href");
+      if (href && (!href.includes("/wiki/") || href.includes("File:"))) {
+        el.remove();
+      }
+    });
+
+    // remove all comments and multiline comments
+    doc.querySelectorAll("body").forEach((el) => {
+      el.innerHTML = el.innerHTML.replace(/<!--[\s\S]*?-->/g, "");
+    });
+
+    // compress the whitespace in the html
+    doc.querySelectorAll("body").forEach((el) => {
+      el.innerHTML = el.innerHTML.replace(/\s+/g, " ");
+    });
+
+    // download the html of all linked pages
+    const links = doc.querySelectorAll("a");
+
+    for (let i = 0; i < links.length; i++) {
+      const link = links[i];
+      const href = link.getAttribute("href");
+      if (href && href.includes("/wiki/")) {
+        const url = new URL(href, "https://en.wikipedia.org");
+        const title = url.pathname.split("/").pop();
+        if (!title) throw new Error("missing title");
+
+        const apiUrl = new URL("https://en.wikipedia.org/w/api.php");
+        apiUrl.search = new URLSearchParams({
+          action: "parse",
+          format: "json",
+          prop: "text",
+          redirect: "true",
+          page: title, //
+          origin: "*",
+        }).toString();
+
+        const response = await fetch(apiUrl);
+
+        // sleep 0.2 - 1 seconds
+        await new Promise((resolve) =>
+          setTimeout(resolve, Math.random() * 800 + 200)
+        );
+        if (!response.ok) {
+          throw new Error(
+            `HTTP error! Status: ${response.status}, ${response.statusText}, ${apiUrl}`
+          );
+        }
+
+        const data = await response.json();
+        const page = data.parse;
+
+        if (!page) {
+          continue;
+        }
+
+        // parse the html
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(page.text["*"], "text/html");
+
+        // remove all images
+        doc.querySelectorAll("img, style").forEach((el) => el.remove());
+
+        // remove all css classes
+        doc.querySelectorAll("*").forEach((el) => {
+          el.removeAttribute("class");
+        });
+
+        // remove all targets from all links
+        doc.querySelectorAll("a").forEach((el) => {
+          el.removeAttribute("target");
+        });
+
+        const blob = new Blob([doc.documentElement.innerHTML], {
+          type: "text/html",
+        });
+
+        zip.folder("wiki")?.file(`${title}.html`, blob);
+      }
+    }
+
+    // add a "./" prefix to all links that contain /wiki/
+    doc.querySelectorAll("a").forEach((el) => {
+      const href = el.getAttribute("href");
+      if (href && href.includes("/wiki/")) {
+        el.setAttribute("href", `./${href}.html`);
+      }
+    });
+
+    const blob = new Blob([doc.documentElement.innerHTML], {
+      type: "text/html",
+    });
+
+    zip.file(`${title}.html`, blob);
+
+    const content = await zip.generateAsync({ type: "blob" });
+
+    saveAs(content, `${title}.zip`);
+
+    return blob;
   }
 
   async function isValidWikipediaUrl(urlString: string): Promise<boolean> {
@@ -24,7 +181,10 @@ export default function Home() {
         initialValues={{ wikiUrl: "" }}
         onSubmit={async (values, { setSubmitting }) => {
           if (await isValidWikipediaUrl(values.wikiUrl)) {
-            await dostuff(values);
+            const url = new URL(values.wikiUrl);
+            await getWikipediaHtml(url);
+          } else {
+            alert("invalid wikipedia url");
           }
           setSubmitting(false);
         }}
@@ -38,6 +198,12 @@ export default function Home() {
               }
             }}
           >
+            {isSubmitting && (
+              <svg
+                className="... mr-3 h-5 w-5 animate-spin"
+                viewBox="0 0 24 24"
+              ></svg>
+            )}
             <Field
               className={"border border-slate-500 p-1 "}
               type="text"
